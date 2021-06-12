4323b7121d9fcb43dac7a71d9a47e4bf5bc3f12f
ssum-dal <min8226@gmail.com>
2021-06-12 13:05:30 +0900

    delete github strategy

--- src/passport.js
+++ src/passport.js
@@ -1,19 +1,9 @@
 import passport from "passport";
-import GithubStrategy from "passport-github";
 import NaverStrategy from "passport-naver";
 import KakaoStrategy from "passport-kakao";
 import User from "./models/User";
-import { githubLoginCallback, naverLoginCallback, kakaoLoginCallback } from "./controllers/userController";
+import { naverLoginCallback, kakaoLoginCallback } from "./controllers/userController";
 
-
-passport.use(
-    new GithubStrategy({
-        clientID: process.env.GH_ID,
-        clientSecret: process.env.GH_SECRET,
-        callbackURL:`http://localhost:4000/auth/github/callback`
-    },
-    githubLoginCallback)   
-);
 
 passport.use(
     new NaverStrategy({
